4dd2ddc8278aea16f6cbc932973dbe53a3881af2
James Hughes <jmhghs7@gmail.com>
2016-06-21 18:54:17 -0700

    Added HTML and JS

---
+++ script.js
@@ -0,0 +1,148 @@
+//Step 1
+//var someMonth;
+//function theMonth
+//currentMonth
+//var summerMonth;
+//function myLibrary
+
+//Step 2
+//numeric literal expression: 5
+//string literal expression: "This is a string."
+//Boolean expression: true
+//null literal expression: null
+
+//Step 3
+//var someMonth='March';
+//var someString="Hello" + " " + name;
+
+//Step 4
+//str-var firstName;
+//str-var lastName;
+//str-var address;
+//str-var city;
+//str-var state;
+//str-var zip;
+//str-var yourAge;
+//str-var referalSource;
+//str-var contact;
+
+//Step 5
+/*str-var firstName;
+firstName="Bob";*/
+/*str-var lastName;
+lastName="Smith";*/
+/*str-var address;
+address="1234 Main Street";*/
+//str-var city="Anytown";
+//str-var state="CA";
+//str-var zip="12345-6789", yourAge="35";
+//str-var referalSource="radio ad", contact="yes";
+
+//Step 6
+/*var myAge=3;
+console.log('I a only' + ' ' + myAge + ' ' + 'years old.');*/
+
+/*var y;
+y="The Bible is";
+var x;
+x=true;
+console.log(y + " " + x + "!");*/
+
+/*var two=3;
+var three=3;
+console.log(two==three);*/
+
+//Step 7
+/*console.log('The' + ' ' + orangePrimate +' ' + 'is greatly endangered.');
+var orangePrimate='orangutan';*/
+
+//Step 8
+/*var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+console.log(someString);*/
+
+//Step 9
+/*var x = 'valid';
+x = null;
+console.log('This contract is' + ' ' + x + ' ' + 'and void!');*/
+
+/*var model;
+console.log('That  car is a' +' ' + model + '.');*/
+
+//Step 10
+//var x='some text';
+//var y=3;
+//var z=false;
+//var xx=['Monday', 'Tuesday', 'Wednesday'];
+//var yy;
+//console.log(typeof x);
+//console.log(typeof y);
+//console.log(typeof z);
+//console.log(typeof xx);
+//console.log(typeof yy);
+
+//Step 11
+/*var name='James Hughes';
+alert('Hello ' + name + ', welcome to the JavaScript class!');*/
+
+//Step 12
+/*var name='James Hughes';
+alert('Hello ' + name + ', welcome to the JavaScript class!');*/
+
+//Step 13
+/*var name='James Hughes';
+var course="Javascript";
+alert('Hello ' + name + ', welcome to the ' + course + ' class!');*/
+
+//Step 14
+/*var name='James Hughes';
+var course="Javascript";
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');*/
+
+//Step 15
+/*var name=prompt('What is your name?');
+var course="Javascript";
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');*/
+
+//Step 16
+/*var name=prompt('What is your name?');
+var course=prompt('What class are you taking?');
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');*/
+
+//Step 17
+/*var x=10;
+var y=20;
+console.log(x+y);*/
+
+//Step 18
+/*var x=20;
+console.log(x+=20);*/
+
+//Step 19
+/*var x=20;
+console.log(x*=5);*/
+
+//Step 20
+/*var x=20%3;
+console.log(x/=1);*/
+
+//Step 21
+/*var x=10;
+var y=5;
+console.log(x==y||x!=y);*/
+
+//Step 22
+/*var x=5;
+var y=15;
+console.log(x>=y);*/
+
+//Step 23
+/*var widgit=new Object();
+console.log(typeof widgit);*/
+
+//Step 24
+/*var widgit=new Object();
+console.log(instanceof object);*/
+
+//Step 25
+var widgit=new Object();
+console.log(instanceof Boolean);
